96107e0105cd4409dfe6309c49c6f97108573b59
Xander Levy <xanderslevy@gmail.com>
2024-08-28 19:42:25 +0000

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,11 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
-	<main>
+	<div class="main">
 		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	</div>
 </div>
 
 <style>
@@ -20,34 +13,16 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
+		align-items: center;
+		justify-content: center;
 	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
+	.main{
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		width: 100%;
+		height: 100vh;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,66 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="XANDER LEVY" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
+<div class="card mainshit">
+	<div class="container">
+		<div style="display: flex; flex-direction: column; justify-content:start; width: 100%">
+			<h1>
+				XANDER LEVY
+			</h1>
+			<p>
+			Art portfolio
+			</p>
+		</div>
+		<div style="margin-top: 25px; width: 100%">
+			<h3 style="width: 100%; text-align: start; margin: 0px; ">Genreal Statement</h3>
+			<p>my art sucks, but that dosen't matter. commonly, people value art by the skill level taked to craft it, which is admerbiale, but it shouldn't be used to assign value to the art. people like to judge modern art, they say it isn't art. thats what intrests me; what can we call art, how do we define it? if it's not skill, or artist vision, what is it?</p>
+		</div>
+	</div>
+	<div class="projects">
+	<p>project 1</p>
+	<p>project 2</p>
+	<p>project 3</p>
+	</div>
+</div>
 <style>
-	section {
+.projects{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 20px;
+}
+.projects p{
+	border-radius: 2px;
+	padding: 5px;
+	cursor: pointer;
+	text-decoration: underline;
+}
+	.container{
+		width: 100%; height:100%;
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 0.6;
+		justify-content: center;
+		padding: 20px;
+		flex-direction: row;
 	}
 
-	h1 {
-		width: 100%;
+		.mainshit{
+		width: 80%; height:80%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 20px;
+		flex-direction: column;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	h1, p{
+		padding: 0px;
+		margin: 0px;
 	}
 </style>
